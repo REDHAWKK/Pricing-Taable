132ec55a050e4f6072621072b488ed991145d640
REDHAWKK <Princefrosh2007@gmail.com>
2024-09-26 09:42:14 -0700

    new welcome page as the landing page

--- Wine.js
+++ Wine.js
@@ -12,3 +12,30 @@ window.onload = function () {
     });
   };
   
+  //SCHOOL PRIDE CONFETTI CODE*
+  window.onload = function() {
+    var end = Date.now() + 6 * 1000; // Run the confetti for 5 seconds
+
+  var colors = [ '#453db8', '#e6cec6', '#dfb8ab', '#fff' ];
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }, // Left side of the screen
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }, // Right side of the screen
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
+};
+  /*END OF SCHOOL PRIDE CONFETTI CODE*/1
