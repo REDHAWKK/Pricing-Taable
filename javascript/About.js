68e440fcfed56370c88da525ae032d91ec226443
REDHAWKK <Princefrosh2007@gmail.com>
2024-10-30 15:26:35 -0700

    about change ready
    I have fully built, designed and created a fully functional about page

--- javascript/About.js
+++ javascript/About.js
@@ -1,34 +1,11 @@
-var duration = 15 * 1000;
-var animationEnd = Date.now() + duration;
-var defaults = { 
-  startVelocity: 30, 
-  spread: 360, 
-  ticks: 60, 
-  zIndex: 0,
-  colors: ['#453db8', '#ffffff']  // Set the colors
-};
-
-function randomInRange(min, max) {
-  return Math.random() * (max - min) + min;
+const hiddenElements = document.querySelectorAll('.hidden');
+function scrollHandler() {
+  hiddenElements.forEach(element => {
+  const rect = element.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  if (rect.top <= windowHeight - 100) {
+    element.classList.add('show');
 }
-
-var interval = setInterval(function() {
-  var timeLeft = animationEnd - Date.now();
-
-  if (timeLeft <= 0) {
-    return clearInterval(interval);
-  }
-
-  var particleCount = 50 * (timeLeft / duration);
-  // since particles fall down, start a bit higher than random
-  confetti({ 
-    ...defaults, 
-    particleCount, 
-    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } 
-  });
-  confetti({ 
-    ...defaults, 
-    particleCount, 
-    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } 
-  });
-}, 250);
+});
+}
+window.addEventListener('scroll', scrollHandler)
